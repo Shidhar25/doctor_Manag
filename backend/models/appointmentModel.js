c0452a06dc3f1ec06ee721ab8eeb688c53e37618
aditya <adityakotkar47@gmail.com>
2025-02-22 14:30:27 +0530

    fix: improve controllers

--- backend/models/appointmentModel.js
+++ backend/models/appointmentModel.js
@@ -14,5 +14,5 @@ const appointmentSchema = new mongoose.Schema({
     isCompleted: { type: Boolean, default: false }
 })
 
-const appointmentModel = mongoose.models.appointment || mongoose.model("appointment", appointmentSchema)
+const appointmentModel = mongoose.model("appointment", appointmentSchema)
 export default appointmentModel
